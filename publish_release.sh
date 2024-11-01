c6d5dddc9bba59779cb46571853cf60ef06b6c57
John Hotovy <jghotovy92@gmail.com>
2024-11-01 13:12:19 -0600

    Add TODO to documentation for publishing script

--- publish_release.sh
+++ publish_release.sh
@@ -3,6 +3,9 @@ set -o xtrace
 
 rm -rf build/
 
+# TODO: ensure the correct cmake version is being used (from the NDK)
+# this might not work if cmake is already on the path before sourcing
+# build_environment.sh
 source ./build_environment.sh
 ./prefab_build.sh
 
